6210824966038e9412a5689d41b080ed2a92d285
seandassoff <dassoffsean@yahoo.com>
2020-04-08 12:30:53 +0500

    feat: template

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+//Setup Routes
+
+// Index Redirect
+router.get("/", function (req, res) {
+  res.redirect("/index");
+});
+
+// Index Page
+router.get("/index", function (req, res) {
+  burger.selectAll(function (data) {
+    const burgers = data.filter((d) => !d.devoured);
+    const devouredBurgers = data.filter((d) => d.devoured);
+    var hbsObject = { burgers, devouredBurgers };
+    //console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+// Create a New Burger
+router.post("/burger/create", function (req, res) {
+  burger.insertOne(req.body.burger_name, function () {
+    res.redirect("/index");
+  });
+});
+
+// Devour a Burger
+router.post("/burger/eat/:id", function (req, res) {
+  burger.updateOne(req.params.id, function () {
+    res.redirect("/index");
+  });
+});
+
+// Export routes
+module.exports = router;
